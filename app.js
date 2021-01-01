d03e766ebc5d1e958218eb9eccdf2a895f59a92f
eddymandran <eddy.mandran@gmail.com>
2021-01-01 19:07:25 +0100

    creation des routes permettant de faire une recherche sur le nom et d'ajouter un utilisateurs

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const connection = require("./connection");
+const connection = require('./connection');
 
 const app = express();
 app.use(express.json());
-
 
 // route principal pour l'accueil
 app.get('/', (req, res) => {
@@ -21,50 +20,123 @@ app.get('/api/users/', (req, res) => {
     }
   });
 });
-
-
-
-
-// route pour récupérer tous les utilisateurs avec un filtre "commence par ..." sur la colonne nom
-app.get('/api/users/filteredby/nameStart', (req, res) => {
-  const startLetters = `%${req.query.name}`;
-  connection.query('SELECT * FROM myTable WHERE nom LIKE ?', startLetters,
-   (err, results) => {
-    if (err) {
-      return res.status(500).json({ err: err.message, sql: err.sql });
-    } if (results.length === 0) {
-      res.status(200).send("No users match");
+// route pour récupérer tous les utilisateurs avec uniquement le nom, prenom et la date d'inscription
+app.get('/api/users/light', (req, res) => {
+  connection.query(
+    'SELECT nom, prenom, date_inscription FROM myTable',
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error retrieving data');
+      } else {
+        res.status(200).json(results);
+      }
     }
-    return res.status(200).json(results);
-  }
-  );
-}); 
-
-// route pour récupérer tous les utilisateurs avec un filtre "superieur à ..." sur la colonne nbre_followers
-app.get('/api/users/filteredby/followers', (req, res) => {
-  connection.query('SELECT * FROM myTable WHERE nbre_followers > ?', req.query.nbre_followers,
-   (err, results) => {
-    if (err) {
-      return res.status(500).json({ err: err.message, sql: err.sql });
-    } if (results.length === 0) {
-      res.status(200).send("No users match");
-    }
-    return res.status(200).json(results);
-  }
-  );
-}); 
-
-// route permettant de récupérer les données ordonnées par date d'inscription
-app.get("/api/users/:order", (req, res) => {
-  const orderBy = req.params.order;
-  connection.query(`SELECT * FROM myTable ORDER BY date_inscription ${orderBy}`, (err, results) => {
-    if (err) {
-      return res.status(500).json({ err: err.message, sql: err.sql });
-    } 
-    return res.status(200).json(results);
-  }
   );
 });
 
+// route pour récupérer tous les utilisateurs avec un filtre "contient ..." sur la colonne nom
+app.get('/api/users/nameContains', (req, res) => {
+  const name = `%${req.query.name}%`;
+  connection.query(
+    'SELECT * FROM myTable WHERE nom LIKE ?',
+    name,
+    (err, results) => {
+      if (err) {
+        return res.status(500).json({ err: err.message, sql: err.sql });
+      }
+      if (results.length === 0) {
+        res.status(200).send('No users match');
+      }
+      return res.status(200).json(results);
+    }
+  );
+});
+
+// route pour récupérer tous les utilisateurs avec un filtre "commence par ..." sur la colonne nom
+app.get('/api/users/nameStart', (req, res) => {
+  const name = `${req.query.name}%`;
+  connection.query(
+    'SELECT * FROM myTable WHERE nom LIKE ?',
+    name,
+    (err, results) => {
+      if (err) {
+        return res.status(500).json({ err: err.message, sql: err.sql });
+      }
+      if (results.length === 0) {
+        res.status(200).send('No users match');
+      }
+      return res.status(200).json(results);
+    }
+  );
+});
+
+// route pour récupérer tous les utilisateurs avec un filtre "superieur à ..." sur la colonne nbre_followers
+app.get('/api/users/followers', (req, res) => {
+  connection.query(
+    'SELECT * FROM myTable WHERE nbre_followers > ?',
+    req.query.nbre_followers,
+    (err, results) => {
+      if (err) {
+        return res.status(500).json({ err: err.message, sql: err.sql });
+      }
+      if (results.length === 0) {
+        res.status(200).send('No users match');
+      }
+      return res.status(200).json(results);
+    }
+  );
+});
+
+// route permettant de récupérer les données ordonnées par date d'inscription
+app.get('/api/users/:order', (req, res) => {
+  const orderBy = req.params.order;
+  connection.query(
+    `SELECT * FROM myTable ORDER BY date_inscription ${orderBy}`,
+    (err, results) => {
+      if (err) {
+        return res.status(500).json({ err: err.message, sql: err.sql });
+      }
+      return res.status(200).json(results);
+    }
+  );
+});
+
+// route pour ajouter un nouveau utilisateur
+app.post('/api/users', (req, res) => {
+  connection.query(
+    'SELECT * FROM myTable WHERE nom = ?',
+    req.body.nom,
+    (err, results) => {
+      if (err) {
+        return res.status(500).json({ err: err.message, sql: err.sql });
+      }
+      if (results.length !== 0) {
+        return res.status(422).send('This user already exist');
+      }
+      connection.query(
+        'INSERT INTO myTable SET ?',
+        req.body,
+        (err2, results2) => {
+          if (err2) {
+            return res.status(500).json({ err: err2.message, sql: err2.sql });
+          }
+          connection.query(
+            'SELECT * FROM myTable WHERE id = ?',
+            results2.insertId,
+            (err3, results3) => {
+              if (err3) {
+                return res
+                  .status(500)
+                  .json({ err: err3.message, sql: err3.sql });
+              }
+              res.status(201).json(results3[0]);
+            }
+          );
+        }
+      );
+    }
+  );
+});
 
 app.listen(5000, () => console.log('server listening on port 5000'));
